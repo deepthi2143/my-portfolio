0ad896a4b833d4ad8d931f2de06b6a94a776e242
deepthi2143 <167744399+deepthi2143@users.noreply.github.com>
2024-05-07 06:46:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Front End Developer", "Competative Programmer", "Power BI Developer","Big Data Analyst","SQL Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function openCertificate(certificate) {
+    window.open(certificate);
+}
+function opentab(certificate) {
+    window.open(certificate);
+}
